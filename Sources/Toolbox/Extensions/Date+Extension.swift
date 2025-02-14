57ee0d1fd7d6c2eca1285a8b4ae34dbc492fe1d3
Sambit Das <sambit650@gmail.com>
2025-02-15 01:00:08 +0530

    add date helper methods and unit tests

--- Sources/Toolbox/Extensions/Date+Extension.swift
+++ Sources/Toolbox/Extensions/Date+Extension.swift
@@ -7,13 +7,56 @@
 
 import Foundation
 
+// MARK: - Helper variables
+
 extension Date {
 
-  public func formateDateDDMMYYYY() -> String {
-    let date = self
+  /// Gets the beginning of a day from a given date.
+  public var startOfDay: Date {
+    return Calendar.current.startOfDay(for: self)
+  }
+
+  /// Gets the end of a day from a given date.
+  public var endOfDay: Date {
+    return Calendar.current.date(byAdding: .second, value: 86399, to: startOfDay)!
+  }
+
+  /// Check If a Date is in Today
+  public var isToday: Bool {
+    Calendar.current.isDateInToday(self)
+  }
+
+  /// Check If a Date is in Yesterday
+  public var isYesterday: Bool {
+    Calendar.current.isDateInYesterday(self)
+  }
+
+  /// Check If a Date is in Tomorrow
+  public var isTomorrow: Bool {
+    Calendar.current.isDateInTomorrow(self)
+  }
+}
+
+// MARK: - Helper Methods
+
+extension Date {
+
+  /// Date Formatter.
+  public func formatted(_ format: String = "yyyy-MM-dd HH:mm") -> String {
     let formatter = DateFormatter()
-    formatter.dateFormat = "dd MMM yyyy"
-    let currentDate = formatter.string(from: date)
-    return currentDate
+    formatter.dateFormat = format
+    return formatter.string(from: self)
+  }
+
+  /// Easily modify dates by adding or subtracting time components.
+  public func adding(_ component: Calendar.Component, value: Int) -> Date {
+    return Calendar.current.date(byAdding: component, value: value, to: self) ?? self
+  }
+
+  /// Get Weekday Name from a Date // "Monday", "Tuesday", etc.
+  public func dayOfWeek() -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "EEEE"  // "Monday", "Tuesday", etc.
+    return formatter.string(from: self)
   }
 }
--- Tests/ToolboxTests/Extensions/Date+ExtensionTests.swift
+++ Tests/ToolboxTests/Extensions/Date+ExtensionTests.swift
@@ -13,7 +13,7 @@ import Testing
 @MainActor
 struct DateExtensionTests {
 
-  @Test("formateDateDDMMYYYY", arguments: [
+  @Test("formatted", arguments: [
     ("2025/02/02", "02 Feb 2025"),
     ("2025/12/25", "25 Dec 2025")
   ])
@@ -24,9 +24,139 @@ struct DateExtensionTests {
     let date = dateFormatter.date(from: input.dateString)!
 
     // When: We format the date using the formateDateDDMMYYYY method
-    let formattedDate = date.formateDateDDMMYYYY()
+    let expectedFormat = "dd MMM yyyy"
+    let formattedDate = date.formatted(expectedFormat)
 
     // Then
     #expect(formattedDate == input.expected)
+  }
+
+  @Test("adding", arguments: [
+    ((dateString: "2025/02/10", component: Calendar.Component.day, value: 5), "2025/02/15"),
+    ((dateString: "2025/12/25", component: Calendar.Component.month, value: -1), "2025/11/25")
+  ])
+  func test_addingDateComponent(input: ((dateString: String, component: Calendar.Component, value: Int), expected: String)) async throws {
+    // Given
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy/MM/dd"
+    let date = dateFormatter.date(from: input.0.dateString)!
+
+    // When: We add/subtract the component
+    let modifiedDate = date.adding(input.0.component, value: input.0.value)
+    let formattedModifiedDate = dateFormatter.string(from: modifiedDate)
+
+    // Then
+    #expect(formattedModifiedDate == input.1)
+  }
+
+  @Test("dayOfWeek", arguments: [
+    ("2025/02/17", "Monday"),
+    ("2025/02/18", "Tuesday"),
+    ("2025/12/25", "Thursday")
+  ])
+  func test_dayOfWeek(input: (dateString: String, expected: String)) async throws {
+    // Given
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy/MM/dd"
+    let date = dateFormatter.date(from: input.dateString)!
+
+    // When: We get the weekday name
+    let weekday = date.dayOfWeek()
+
+    // Then
+    #expect(weekday == input.expected)
+  }
+}
+
+extension DateExtensionTests {
+
+  @Test("startOfDay", arguments: [
+    ("2025/02/15 14:30:00", "2025/02/15 00:00:00"),
+    ("2025/12/25 23:59:59", "2025/12/25 00:00:00")
+  ])
+  func test_startOfDay(input: (dateString: String, expected: String)) async throws {
+    // Given
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+    let date = formatter.date(from: input.dateString)!
+
+    // When
+    let start = date.startOfDay
+    let formattedStart = formatter.string(from: start)
+
+    // Then
+    #expect(formattedStart == input.expected)
+  }
+
+  @Test("endOfDay", arguments: [
+    ("2025/02/15 10:00:00", "2025/02/15 23:59:59"),
+    ("2025/12/25 00:00:00", "2025/12/25 23:59:59")
+  ])
+  func test_endOfDay(input: (dateString: String, expected: String)) async throws {
+    // Given
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+    let date = formatter.date(from: input.dateString)!
+
+    // When
+    let end = date.endOfDay
+    let formattedEnd = formatter.string(from: end)
+
+    // Then
+    #expect(formattedEnd == input.expected)
+  }
+
+  @Test("isToday", arguments: [
+    ("2025/02/15", true),
+    ("2025/02/14", false),
+    ("2025/02/16", false)
+  ])
+  func test_isToday(input: (dateString: String, expected: Bool)) async throws {
+    // Given
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/MM/dd"
+    let date = formatter.date(from: input.dateString)!
+
+    // When
+    let result = date.isToday
+
+    // Then
+    #expect(result == input.expected)
+  }
+
+  @Test("isYesterday", arguments: [
+    ("2025/02/14", true),
+    ("2025/02/15", false),
+    ("2025/02/16", false)
+  ])
+  func test_isYesterday(input: (dateString: String, expected: Bool)) async throws {
+    // Given
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/MM/dd"
+    let date = formatter.date(from: input.dateString)!
+
+    // When
+    let result = date.isYesterday
+
+    // Then
+    #expect(result == input.expected)
+  }
+
+  @Test("isTomorrow", arguments: [
+    ("2025/02/16", true),
+    ("2025/02/15", false),
+    ("2025/02/14", false)
+  ])
+  func test_isTomorrow(input: (dateString: String, expected: Bool)) async throws {
+    // Given
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/MM/dd"
+    let date = formatter.date(from: input.dateString)!
+
+    // When
+    let result = date.isTomorrow
+
+    // Then
+    #expect(result == input.expected)
   }
 }
