9384a336e96586c6ecc8cca65154be314d0b6129
Sambit Das <sambit650@gmail.com>
2025-07-06 11:17:57 +0530

    update unit tests

--- Tests/ToolboxTests/Utility/StringUtilsTests.swift
+++ Tests/ToolboxTests/Utility/StringUtilsTests.swift
@@ -18,13 +18,39 @@ struct StringUtilsTests {
     ("test123 ", "test123")
   ])
   func test_trimWhitespace(input: (text: String, expected: String)) async throws {
-    // GIVEN
-    let utils = StringUtils()
 
     // WHEN
-    let isContainsOnlyDigits = utils.trimWhitespace(input.text)
+    let result = StringUtils.trimWhitespace(input.text)
 
     // THEN
-    #expect(isContainsOnlyDigits == input.expected)
+    #expect(result == input.expected)
+  }
+
+  @Test("removingWhitespaces", arguments: [
+    (" 4345 ", "4345"),
+    (" 000", "000"),
+    ("test123 ", "test123")
+  ])
+  func test_removingWhitespaces(input: (text: String, expected: String)) async throws {
+
+    // WHEN
+    let result = StringUtils.removingWhitespaces(from: input.text)
+
+    // THEN
+    #expect(result == input.expected)
+  }
+
+  @Test("removingCharacters", arguments: [
+    ("He11o W0r1d!", "\\d", "Heo Wrd!"),
+    ("Hello@# World!", "[^a-zA-Z0-9 ]", "Hello World"),
+    (" H e l l o ", "\\s", "Hello")
+  ])
+  func test_removingCharacters(input: (text: String, pattern: String, expected: String)) async throws {
+
+    // WHEN
+    let result = StringUtils.removingCharacters(from: input.text, matching: input.pattern)
+
+    // THEN
+    #expect(result == input.expected)
   }
 }
